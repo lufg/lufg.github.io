35955bb2e64887d5466b3a34c91f98f6c4f02a1a
Frank <frank.lufg@gmail.com>
2026-02-09 21:06:39 +0800

    Update site title, rename posts folder, remove graph

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "LuFrank",
+    pageTitle: "路边的风",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
