b54fcc3612f9e9494625f9e0dcca2a49769ed7db
Frank <frank.lufg@gmail.com>
2026-02-13 21:04:03 +0800

    chore: update page title to Frank's Blog

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Frank的留声机",
+    pageTitle: "Frank's Blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
